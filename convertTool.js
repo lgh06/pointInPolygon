9bb6ec0118933c00b6b7a12b93223376c97027bd
hnnk <i@passby.me>
2026-01-19 11:07:46 +0800

    移除this.

--- convertTool.js
+++ convertTool.js
@@ -11,17 +11,17 @@ const ee = 0.00669342162296594323
 
 function transformLon(x, y) {
     var ret = 300.0 + x + 2.0 * y + 0.1 * x * x + 0.1 * x * y + 0.1 * Math.sqrt(Math.abs(x));
-    ret += (20.0 * Math.sin(6.0 * x * this.pi) + 20.0 * Math.sin(2.0 * x * this.pi)) * 2.0 / 3.0;
-    ret += (20.0 * Math.sin(x * this.pi) + 40.0 * Math.sin(x / 3.0 * this.pi)) * 2.0 / 3.0;
-    ret += (150.0 * Math.sin(x / 12.0 * this.pi) + 300.0 * Math.sin(x / 30.0 * this.pi)) * 2.0 / 3.0;
+    ret += (20.0 * Math.sin(6.0 * x * pi) + 20.0 * Math.sin(2.0 * x * pi)) * 2.0 / 3.0;
+    ret += (20.0 * Math.sin(x * pi) + 40.0 * Math.sin(x / 3.0 * pi)) * 2.0 / 3.0;
+    ret += (150.0 * Math.sin(x / 12.0 * pi) + 300.0 * Math.sin(x / 30.0 * pi)) * 2.0 / 3.0;
     return ret;
 }
 
 function transformLat(x, y) {
     var ret = -100.0 + 2.0 * x + 3.0 * y + 0.2 * y * y + 0.1 * x * y + 0.2 * Math.sqrt(Math.abs(x));
-    ret += (20.0 * Math.sin(6.0 * x * this.pi) + 20.0 * Math.sin(2.0 * x * this.pi)) * 2.0 / 3.0;
-    ret += (20.0 * Math.sin(y * this.pi) + 40.0 * Math.sin(y / 3.0 * this.pi)) * 2.0 / 3.0;
-    ret += (160.0 * Math.sin(y / 12.0 * this.pi) + 320 * Math.sin(y * this.pi / 30.0)) * 2.0 / 3.0;
+    ret += (20.0 * Math.sin(6.0 * x * pi) + 20.0 * Math.sin(2.0 * x * pi)) * 2.0 / 3.0;
+    ret += (20.0 * Math.sin(y * pi) + 40.0 * Math.sin(y / 3.0 * pi)) * 2.0 / 3.0;
+    ret += (160.0 * Math.sin(y / 12.0 * pi) + 320 * Math.sin(y * pi / 30.0)) * 2.0 / 3.0;
     return ret;
 }
 // 判断是否在国内
@@ -40,21 +40,21 @@ function outOfChina(lat, lon) {
 export const wgs_gcj_encrypts = (latlons) => {
     var point = [];
     for (const latlon of latlons) {
-        if (this.outOfChina(latlon.lat, latlon.lng)) {
+        if (outOfChina(latlon.lat, latlon.lng)) {
             point.push({
                 lat: latlon.lat,
                 lng: latlon.lng
             })
             return point;
         }
-        var dLat = this.transformLat(latlon.lng - 105.0, latlon.lat - 35.0);
-        var dLon = this.transformLon(latlon.lng - 105.0, latlon.lat - 35.0);
-        var radLat = latlon.lat / 180.0 * this.pi;
+        var dLat = transformLat(latlon.lng - 105.0, latlon.lat - 35.0);
+        var dLon = transformLon(latlon.lng - 105.0, latlon.lat - 35.0);
+        var radLat = latlon.lat / 180.0 * pi;
         var magic = Math.sin(radLat);
-        magic = 1 - this.ee * magic * magic;
+        magic = 1 - ee * magic * magic;
         var sqrtMagic = Math.sqrt(magic);
-        dLat = (dLat * 180.0) / ((this.a * (1 - this.ee)) / (magic * sqrtMagic) * this.pi);
-        dLon = (dLon * 180.0) / (this.a / sqrtMagic * Math.cos(radLat) * this.pi);
+        dLat = (dLat * 180.0) / ((a * (1 - ee)) / (magic * sqrtMagic) * pi);
+        dLon = (dLon * 180.0) / (a / sqrtMagic * Math.cos(radLat) * pi);
         var lat = latlon.lat + dLat;
         var lng = latlon.lng + dLon;
         point.push({
@@ -74,8 +74,8 @@ export const bd_google_encrypt = (latlons) => {
     for (const latlon of latlons) {
         var x = latlon.lng - 0.0065;
         var y = latlon.lat - 0.006;
-        var z = Math.sqrt(x * x + y * y) - 0.00002 * Math.sin(y * this.x_pi);
-        var theta = Math.atan2(y, x) - 0.000003 * Math.cos(x * this.x_pi);
+        var z = Math.sqrt(x * x + y * y) - 0.00002 * Math.sin(y * x_pi);
+        var theta = Math.atan2(y, x) - 0.000003 * Math.cos(x * x_pi);
         var gg_lon = z * Math.cos(theta);
         var gg_lat = z * Math.sin(theta);
         point.push({
@@ -95,8 +95,8 @@ export const google_bd_encrypt = (latlons) => {
     for (const latlon of latlons) {
         var x = latlon.lng;
         var y = latlon.lat;
-        var z = Math.sqrt(x * x + y * y) + 0.00002 * Math.sin(y * this.x_pi);
-        var theta = Math.atan2(y, x) + 0.000003 * Math.cos(x * this.x_pi);
+        var z = Math.sqrt(x * x + y * y) + 0.00002 * Math.sin(y * x_pi);
+        var theta = Math.atan2(y, x) + 0.000003 * Math.cos(x * x_pi);
         var bd_lon = z * Math.cos(theta) + 0.0065;
         var bd_lat = z * Math.sin(theta) + 0.006;
         point.push({
@@ -114,21 +114,21 @@ export const google_bd_encrypt = (latlons) => {
 export const gcj_wgs_encrypts = (latlons) => {
     var point = [];
     for (const latlon of latlons) {
-        if (this.outOfChina(latlon.lat, latlon.lng)) {
+        if (outOfChina(latlon.lat, latlon.lng)) {
             point.push({
                 lat: latlon.lat,
                 lng: latlon.lng
             })
             return point;
         }
-        var dLat = this.transformLat(latlon.lng - 105.0, latlon.lat - 35.0);
-        var dLon = this.transformLon(latlon.lng - 105.0, latlon.lat - 35.0);
-        var radLat = latlon.lat / 180.0 * this.pi;
+        var dLat = transformLat(latlon.lng - 105.0, latlon.lat - 35.0);
+        var dLon = transformLon(latlon.lng - 105.0, latlon.lat - 35.0);
+        var radLat = latlon.lat / 180.0 * pi;
         var magic = Math.sin(radLat);
-        magic = 1 - this.ee * magic * magic;
+        magic = 1 - ee * magic * magic;
         var sqrtMagic = Math.sqrt(magic);
-        dLat = (dLat * 180.0) / ((this.a * (1 - this.ee)) / (magic * sqrtMagic) * this.pi);
-        dLon = (dLon * 180.0) / (this.a / sqrtMagic * Math.cos(radLat) * this.pi);
+        dLat = (dLat * 180.0) / ((a * (1 - ee)) / (magic * sqrtMagic) * pi);
+        dLon = (dLon * 180.0) / (a / sqrtMagic * Math.cos(radLat) * pi);
         var lat = dLat;
         var lng = dLon;
         point.push({
